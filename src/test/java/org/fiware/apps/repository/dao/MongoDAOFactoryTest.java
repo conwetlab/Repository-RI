5cd48bb6380ef094a11ec1903a2ecb5bb26fa5ff
Javier Ortiz Sancho <jortiz@conwet.com>
2015-05-20 17:24:34 +0200

    Added new tests for RepositorySettings, DAOFactory, MongoDAOFactory and VirtuosoDAOFactory
    Signed-off-by: fdelavega <fdelavega@conwet.com>

---
+++ src/test/java/org/fiware/apps/repository/dao/MongoDAOFactoryTest.java
@@ -0,0 +1,143 @@
+/*
+Modified BSD License
+====================
+
+Copyright (c) 2015, CoNWeTLab, Universidad Politecnica Madrid
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+* Neither the name of the SAP AG nor the
+names of its contributors may be used to endorse or promote products
+derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL SAP AG BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.fiware.apps.repository.dao;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoException;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.fiware.apps.repository.dao.impl.MongoCollectionDAO;
+import org.fiware.apps.repository.dao.impl.MongoResourceDAO;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({MongoDAOFactory.class, Mongo.class, MongoCollectionDAO.class, MongoResourceDAO.class})
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class MongoDAOFactoryTest {
+    
+    MongoDAOFactory toTest;
+    @Mock Mongo mongo;
+    @Mock MongoResourceDAO mongoResourceDAO;
+    @Mock MongoCollectionDAO mongoCollectionDAO;
+    
+    public MongoDAOFactoryTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        mongo = mock(Mongo.class);
+        
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test (expected = MongoException.class)
+    public void createConnection1Test() {
+        try {
+            PowerMockito.whenNew(Mongo.class).withAnyArguments().thenReturn(mongo);
+            when(mongo.getDB(anyString())).thenThrow(UnknownHostException.class);
+        } catch (Exception ex) {
+            fail(ex.getLocalizedMessage());
+        }
+        MongoDAOFactory.createConnection();
+    }
+    
+    @Test (expected = MongoException.class)
+    public void createConnection2Test() {
+        try {
+            PowerMockito.whenNew(Mongo.class).withAnyArguments().thenReturn(mongo);
+            when(mongo.getDB(anyString())).thenThrow(MongoException.class);
+        } catch (Exception ex) {
+            fail(ex.getLocalizedMessage());
+        }
+        MongoDAOFactory.createConnection();
+    }
+    
+    @Test
+    public void createConnection3Test() {
+        try {
+            PowerMockito.whenNew(Mongo.class).withAnyArguments().thenReturn(mongo);
+        } catch (Exception ex) {
+            fail(ex.getLocalizedMessage());
+        }
+        MongoDAOFactory.createConnection();
+        verify(mongo).getDB(anyString());
+    }
+    
+    @Test
+    public void createConnection4Test() {
+        
+        MongoDAOFactory.createConnection();
+        verify(mongo, times(0)).getDB(anyString());
+    }
+    
+    @Test
+    public void getResourceDAOTest() {
+        mongoResourceDAO = mock(MongoResourceDAO.class);
+        try {
+            PowerMockito.whenNew(MongoResourceDAO.class).withNoArguments().thenReturn(mongoResourceDAO);
+        } catch (Exception ex) {
+            fail(ex.getLocalizedMessage());
+        }
+        toTest = new MongoDAOFactory();
+        toTest.getResourceDAO();
+    }
+    
+    @Test
+    public void getCollectionDAOTest() {
+        mongoCollectionDAO = mock(MongoCollectionDAO.class);
+        try {
+            PowerMockito.whenNew(MongoCollectionDAO.class).withNoArguments().thenReturn(mongoCollectionDAO);
+        } catch (Exception ex) {
+            fail(ex.getLocalizedMessage());
+        }
+        toTest = new MongoDAOFactory();
+        toTest.getCollectionDAO();
+    }
+}
---
+++ src/test/java/org/fiware/apps/repository/settings/RepositorySettingsTest.java
@@ -0,0 +1,83 @@
+/*
+Modified BSD License  
+====================
+
+Copyright (c) 2015, CoNWeTLab, UPM
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the SAP AG nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL SAP AG BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.fiware.apps.repository.settings;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import static org.mockito.Matchers.any;
+
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({RepositorySettings.class, Properties.class})
+public class RepositorySettingsTest {
+    
+    Properties properties;
+    
+    public RepositorySettingsTest() {
+    }
+    
+    @Before
+    public void setUp() {
+        properties = mock(Properties.class);
+        try {
+            PowerMockito.whenNew(Properties.class).withNoArguments().thenReturn(properties);
+        } catch (Exception ex) {
+            Logger.getLogger(RepositorySettingsTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Test
+    public void getPropertyExceptionTest() {
+        try {
+            doThrow(IOException.class).when(properties).load(any(InputStream.class));
+        } catch (IOException ex) {
+            fail(ex.getLocalizedMessage());
+        }
+        assertNull(RepositorySettings.getProperty("hola"));
+    }
+    
+    @Test 
+    public void RepositorySettingsConstructorTest() {
+        new RepositorySettings();
+    }
+}
