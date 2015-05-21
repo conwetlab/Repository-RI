353dae29b5dc01aa11cc25dbe2bbaec7f1eb2128
Javier Ortiz Sancho <jortiz@conwet.com>
2015-05-21 12:09:01 +0200

    Added license to MongoDAOFactory
    Signed-off-by: fdelavega <fdelavega@conwet.com>

--- src/main/java/org/fiware/apps/repository/dao/MongoDAOFactory.java
+++ src/main/java/org/fiware/apps/repository/dao/MongoDAOFactory.java
@@ -1,3 +1,32 @@
+/*
+Modified BSD License
+====================
+
+Copyright (c) 2015, CoNWeTLab, Universidad Politecnica Madrid
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+* Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+* Neither the name of the SAP AG nor the
+names of its contributors may be used to endorse or promote products
+derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL SAP AG BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 package org.fiware.apps.repository.dao;
 
 import java.net.UnknownHostException;
@@ -10,43 +39,37 @@ import com.mongodb.DB;
 import com.mongodb.Mongo;
 import com.mongodb.MongoException;
 
-public class MongoDAOFactory extends DAOFactory {	
-	
-	static DB db = null;
-	
-	public ResourceDAO getResourceDAO() {
-		return new MongoResourceDAO();	
-	}
-	
-	
-	public static DB createConnection() {
-		if(db !=null){
-			return db;
-		}
+public class MongoDAOFactory extends DAOFactory {
 
-		
-		Mongo m;
-		try {
-			m = new Mongo(RepositorySettings.getProperty("mongodb.host") , 
-                                Integer.parseInt(RepositorySettings.getProperty("mongodb.port")));
-                        db = m.getDB(RepositorySettings.getProperty("mongodb.db"));			
+    static DB db = null;
 
-		} catch (UnknownHostException e) {
-			System.out.println("Unknown Host: " + e.getMessage());
-			e.printStackTrace();
-		} catch (MongoException e) {
-			System.out.println("MongoDB Error: " + e.getMessage());
-
-		}
-		return db;		
-		
-	}
+    public ResourceDAO getResourceDAO() {
+        return new MongoResourceDAO();
+    }
 
 
-	@Override
-	public CollectionDAO getCollectionDAO() {
-		return new MongoCollectionDAO();
-	}
-	
-	
+    public static DB createConnection() {
+        if(db !=null){
+            return db;
+        }
+
+        Mongo m;
+        try {
+            m = new Mongo(RepositorySettings.getProperty("mongodb.host"),
+                    Integer.parseInt(RepositorySettings.getProperty("mongodb.port")));
+            db = m.getDB(RepositorySettings.getProperty("mongodb.db"));
+        } catch (UnknownHostException | MongoException ex) {
+            throw new MongoException(ex.getLocalizedMessage());
+        }
+        return db;
+
+    }
+
+
+    @Override
+    public CollectionDAO getCollectionDAO() {
+        return new MongoCollectionDAO();
+    }
+
+
 }
