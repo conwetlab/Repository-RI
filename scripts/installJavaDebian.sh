3f6d9ae7fd02daa682e3bb5dd1a9926091f6ac49
fdelavega <fdelavega@conwet.com>
2015-06-08 12:49:31 +0200

    Fix problems saving JAVA_HOME in the environment when the user is not root

--- scripts/installJavaDebian.sh
+++ scripts/installJavaDebian.sh
@@ -5,6 +5,8 @@ sudo apt-get update
 sudo apt-get install -y oracle-java8-installer
 sudo apt-get install -y oracle-java8-set-default
 sudo update-java-alternatives -s java-8-oracle
+
 JAVA_HOME=/usr/lib/jvm/java-8-oracle
 export JAVA_HOME
-sudo echo "JAVA_HOME=\"/usr/lib/jvm/java-8-oracle\"" >> /etc/environment
+
+sudo echo "JAVA_HOME=\"/usr/lib/jvm/java-8-oracle\"" | sudo tee -a /etc/environment > /dev/null
