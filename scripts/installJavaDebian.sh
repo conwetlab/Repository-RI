63c6547cad4da71f76aa325912cbdf6d4a0c2a67
fdelavega <fdelavega@conwet.com>
2015-06-10 12:02:26 +0200

    Fix problems installing Java in minimal Ubuntu/Debian systems

--- scripts/installJavaDebian.sh
+++ scripts/installJavaDebian.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+# Install python software properties in case the command add-apt-repository do not exists
+sudo apt-get install software-properties-common python-software-properties
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install -y oracle-java8-installer
