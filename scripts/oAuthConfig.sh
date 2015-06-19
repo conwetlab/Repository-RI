c711b44d60af8926536ec7d038f5fbbcede478bd
Javier Ortiz Sancho <jortiz@conwet.com>
2015-06-19 13:58:05 +0200

    Updated script to config OAuth from .war file
    Signed-off-by: fdelavega <fdelavega@conwet.com>

--- scripts/oAuthConfig.sh
+++ scripts/oAuthConfig.sh
@@ -12,15 +12,28 @@ while [ $X != "Y" ] && [ $X != "y" ] && [ $X != "N" ] && [ $X != "n" ]; do
 done
 
 if [  $X == "Y" ] || [ $X == "y" ]; then
-	sudo sed -i "s/noSecurity/securityOAuth2/g" $INSPWD/apache-tomcat-8.0.22/webapps/FiwareRepository/WEB-INF/web.xml
+
+	unzip FiwareRepository.war -d temp 	
+
+	sudo sed -i "s/noSecurity/securityOAuth2/g" $INSPWD/temp/WEB-INF/web.xml
 	
-	echo "OAuth2 configuration is located in $INSPWD/apache-tomcat-8.0.22/webapps/FiwareRepository/WEB-INF/classes/properties/repository.properties."
+	echo "OAuth2 configuration is located in $INSPWD/apache-tomcat-8.0.22/webapps/FiwareRepository/WEB-INF/classes/properties/repository.properties"
 
 	echo "What is your FIWAREClient Key?"
 	read X
-	sudo sed -i "s/FIWAREKEY/$X/g" $INSPWD/apache-tomcat-8.0.22/webapps/FiwareRepository/WEB-INF/classes/properties/repository.properties
+	sudo sed -i "/oauth2.key=/c\oauth2.key=$X" $INSPWD/temp/WEB-INF/classes/properties/repository.properties
 	
 	echo "What is your FIWAREClient Secret?"
 	read X
-	sudo sed -i "s/FIWARESECRET/$X/g" $INSPWD/apache-tomcat-8.0.22/webapps/FiwareRepository/WEB-INF/classes/properties/repository.properties
+	sudo sed -i "/oauth2.secret=/c\oauth2.secret=$X" $INSPWD/temp/WEB-INF/classes/properties/repository.properties
+
+	echo "What is your Callback URL?"
+	read X
+	sed -i "/oauth2.callbackURL=/c\oauth2.callbackURL=$X" $INSPWD/temp/WEB-INF/classes/properties/repository.properties
+
+	cd temp
+	zip -r FiwareRepositoryTemp.war *
+	mv ./FiwareRepositoryTemp.war ../FiwareRepository.war
+	cd ..
+	rm -r temp
 fi
