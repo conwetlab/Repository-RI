757590a5ce7ba1b09e354772e8215e01df65bd30
Javier Ortiz Sancho <jortiz@conwet.com>
2015-06-19 13:04:30 +0200

    Updated OAuthConfiguration.sh
    Signed-off-by: fdelavega <fdelavega@conwet.com>

--- OAuthConfiguration.sh
+++ OAuthConfiguration.sh
@@ -8,6 +8,8 @@ echo "This script activate or desactivate OAuth2 authentication in the Repositor
 
 if [ -f "./apache-tomcat-8.0.22/webapps/FiwareRepository/WEB-INF/classes/properties/repository.properties" ]; then
 	REPODIR=$PWD/apache-tomcat-8.0.22/webapps/FiwareRepository
+elif [ -f "$CATALINA_HOME/webapps/FiwareRepository/WEB-INF/classes/properties/repository.properties" ]; then
+	
 else
 	echo "Where is located Repository-RI? Insert the path:"
 	read REPODIR
@@ -30,6 +32,7 @@ while [ $X != "Y" ] && [ $X != "y" ] && [ $X != "N" ] && [ $X != "n" ] && [ $X !
 done
 
 if [  $X == "Y" ] || [ $X == "y" ]; then
+	# Activate oauth2 and configurate
 	echo "OAuth2 configuration is located in $REPODIR/WEB-INF/classes/properties/repository.properties."
 	sed -i "s/noSecurity/securityOAuth2/g" $REPODIR/WEB-INF/web.xml
 
@@ -40,9 +43,16 @@ if [  $X == "Y" ] || [ $X == "y" ]; then
 	echo "What is your FIWAREClient Secret?"
 	read X
 	sed -i "/oauth2.secret=/c\oauth2.secret=$X" $REPODIR/WEB-INF/classes/properties/repository.properties
+
+	echo "What is your Callback URL?"
+	read X
+	sed -i "/oauth2.callbackURL=/c\oauth2.callbackURL=$X" $REPODIR/WEB-INF/classes/properties/repository.properties
+
 elif [  $X == "N" ] || [ $X == "n" ]; then
+	# Desactivate oauth2
 	echo "OAuth2 configuration is located in $REPODIR/WEB-INF/classes/properties/repository.properties."
 	sed -i "s/securityOAuth2/noSecurity/g" $REPODIR/WEB-INF/web.xml
+
 elif [  $X == "C" ] || [ $X == "c" ]; then
 	echo "Configuration aborted"
 fi
