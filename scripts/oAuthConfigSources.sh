28f111b4ce359f4722eeff7a53ecdca4d5ff7278
fdelavega <fdelavega@conwet.com>
2015-06-23 14:29:02 +0200

    Fix problems with configuration files paths, when configuring OAuth2 settings using the source code for the installation

--- scripts/oAuthConfigSources.sh
+++ scripts/oAuthConfigSources.sh
@@ -12,7 +12,7 @@ while [ $X != "Y" ] && [ $X != "y" ] && [ $X != "N" ] && [ $X != "n" ]; do
 done
 
 if [  $X == "Y" ] || [ $X == "y" ]; then
-	sudo sed -i "s/noSecurity/securityOAuth2/g" $INSPWD/apache-tomcat-8.0.22/webapps/FiwareRepository/WEB-INF/web.xml
+	sudo sed -i "s/noSecurity/securityOAuth2/g" $INSPWD/src/main/webapp/WEB-INF/web.xml
 	
 	echo "OAuth2 configuration is located in $INSPWD/apache-tomcat-8.0.22/webapps/FiwareRepository/WEB-INF/classes/properties/repository.properties"
 
@@ -26,5 +26,5 @@ if [  $X == "Y" ] || [ $X == "y" ]; then
 
 	echo "What is your Callback URL?"
 	read X
-	sed -i "/oauth2.callbackURL=/c\oauth2.callbackURL=$X" $REPODIR/src/main/resources/properties/repository.properties
+	sed -i "/oauth2.callbackURL=/c\oauth2.callbackURL=$X" $INSPWD/src/main/resources/properties/repository.properties
 fi
