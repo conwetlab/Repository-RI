292ee0b347aa38d6e002f1841a38dd5a43e8e16e
fdelavega <fdelavega@conwet.com>
2015-06-29 11:09:15 +0200

    Include server configuration in OAuth2 configuration scripts

--- scripts/oAuthConfig.sh
+++ scripts/oAuthConfig.sh
@@ -16,14 +16,29 @@ if [  $X == "Y" ] || [ $X == "y" ]; then
 	unzip FiwareRepository.war -d temp 	
 
 	sudo sed -i "s/noSecurity/securityOAuth2/g" $INSPWD/temp/WEB-INF/web.xml
-	
+
 	echo "OAuth2 configuration is located in $INSPWD/apache-tomcat-8.0.22/webapps/FiwareRepository/WEB-INF/classes/properties/repository.properties"
 
-	echo "What is your FIWAREClient Key?"
+    echo "The default OAuth2 enpoint is http://account.lab.fiware.org"
+    echo "Do you want to provide a different idm enpoint? Y/N"
+    read O
+
+    while [ $O != "Y" ] && [ $O != "y" ] && [ $O != "N" ] && [ $O != "n" ]; do
+        echo "Do you want to provide a different idm enpoint? Y/N"
+        read O
+    done
+
+    if [ $O == "Y" ] || [ $O == "y" ]; then
+        echo "What is the identity manager endpoint?"
+        read X
+        sudo sed -i "/oauth2.server=/c\oauth2.server=$X" $INSPWD/temp/WEB-INF/classes/properties/repository.properties
+    fi
+
+    echo "What is your FIWARE Client id?"
 	read X
 	sudo sed -i "/oauth2.key=/c\oauth2.key=$X" $INSPWD/temp/WEB-INF/classes/properties/repository.properties
-	
-	echo "What is your FIWAREClient Secret?"
+
+	echo "What is your FIWARE Client Secret?"
 	read X
 	sudo sed -i "/oauth2.secret=/c\oauth2.secret=$X" $INSPWD/temp/WEB-INF/classes/properties/repository.properties
 
--- scripts/oAuthConfigSources.sh
+++ scripts/oAuthConfigSources.sh
@@ -13,13 +13,28 @@ done
 
 if [  $X == "Y" ] || [ $X == "y" ]; then
 	sudo sed -i "s/noSecurity/securityOAuth2/g" $INSPWD/src/main/webapp/WEB-INF/web.xml
-	
+
 	echo "OAuth2 configuration is located in $INSPWD/apache-tomcat-8.0.22/webapps/FiwareRepository/WEB-INF/classes/properties/repository.properties"
 
-	echo "What is your FIWAREClient Key?"
+    echo "The default OAuth2 enpoint is http://account.lab.fiware.org"
+    echo "Do you want to provide a different idm enpoint? Y/N"
+    read O
+
+    while [ $O != "Y" ] && [ $O != "y" ] && [ $O != "N" ] && [ $O != "n" ]; do
+        echo "Do you want to provide a different idm enpoint? Y/N"
+        read O
+    done
+
+    if [ $O == "Y" ] || [ $O == "y" ]; then
+        echo "What is the identity manager endpoint?"
+        read X
+        sudo sed -i "/oauth2.server=/c\oauth2.server=$X" $INSPWD/src/main/resources/properties/repository.properties
+    fi
+
+	echo "What is your FIWARE Client id?"
 	read X
 	sudo sed -i "/oauth2.key=/c\oauth2.key=$X" $INSPWD/src/main/resources/properties/repository.properties
-	
+
 	echo "What is your FIWAREClient Secret?"
 	read X
 	sudo sed -i "/oauth2.secret=/c\oauth2.secret=$X" $INSPWD/src/main/resources/properties/repository.properties
