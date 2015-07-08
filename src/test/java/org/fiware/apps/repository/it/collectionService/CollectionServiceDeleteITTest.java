b5e6f671c83d625dc3bf59488efe96da45d1391b
Javier Ortiz Sancho <jortiz@conwet.com>
2015-07-08 17:27:14 +0200

    Updated test files with license
    Signed-off-by: fdelavega <fdelavega@conwet.com>

--- src/test/java/org/fiware/apps/repository/it/collectionService/CollectionServiceDeleteITTest.java
+++ src/test/java/org/fiware/apps/repository/it/collectionService/CollectionServiceDeleteITTest.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.fiware.apps.repository.it.collectionService;
 
 import org.fiware.apps.repository.it.IntegrationTestHelper;
 import java.io.IOException;
-import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 import org.apache.http.Header;
