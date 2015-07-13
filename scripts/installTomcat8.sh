3f60f7e6cd5aedbca55152a217bf88587e08b976
Javier Ortiz Sancho <jortiz@conwet.com>
2015-07-13 18:58:00 +0200

    Updated scripts with new tomcat version (8.0.24)
    Signed-off-by: fdelavega <fdelavega@conwet.com>

--- scripts/installTomcat8.sh
+++ scripts/installTomcat8.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 # Install tomcat 8
-wget http://apache.rediris.es/tomcat/tomcat-8/v8.0.22/bin/apache-tomcat-8.0.22.zip
-unzip apache-tomcat-8.0.22.zip
+wget http://apache.rediris.es/tomcat/tomcat-8/v8.0.24/bin/apache-tomcat-8.0.24.zip
+unzip apache-tomcat-8.0.24.zip
 
-export CATALINA_HOME=$INSPWD/apache-tomcat-8.0.22
-echo "export CATALINA_HOME=\"$INSPWD/apache-tomcat-8.0.22\"" >> ~/.bashrc
+export CATALINA_HOME=$INSPWD/apache-tomcat-8.0.24
+echo "export CATALINA_HOME=\"$INSPWD/apache-tomcat-8.0.24\"" >> ~/.bashrc
 
-chmod +x $INSPWD/apache-tomcat-8.0.22/bin/*.sh
-$INSPWD/apache-tomcat-8.0.22/bin/startup.sh
+chmod +x $INSPWD/apache-tomcat-8.0.24/bin/*.sh
+$INSPWD/apache-tomcat-8.0.24/bin/startup.sh
